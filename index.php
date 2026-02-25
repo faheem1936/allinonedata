391f9ca760a9d90780c093b11f31f96f6913844d
faheem1936 <78230182+faheem1936@users.noreply.github.com>
2026-02-25 20:32:57 +0500

    Update user IDs for API services in index.php

--- index.php
+++ index.php
@@ -26,9 +26,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['ajax'])) {
         "domicile" => ["url"=>"https://mahisite.xyz/user/API/api-domicile.php","userid"=>"12"],
         "kashmir-tree" => ["url"=>"https://mahisite.xyz/user/API/api-kashmirtree.php","userid"=>"12"],
 
-        "fbr" => ["url"=>"https://khoji2.online/user/API/api-fbr.php","userid"=>"150"],
-        "one-click" => ["url"=>"https://khoji2.online/user/API/api-oneclick.php","userid"=>"150"],
-        "all-in-one-details" => ["url"=>"https://khoji2.online/user/API/api-oneclick-cnic.php","userid"=>"150"],
+        "fbr" => ["url"=>"https://khoji2.online/user/API/api-fbr.php","userid"=>"149"],
+        "one-click" => ["url"=>"https://khoji2.online/user/API/api-oneclick.php","userid"=>"149"],
+        "all-in-one-details" => ["url"=>"https://khoji2.online/user/API/api-oneclick-cnic.php","userid"=>"149"],
     ];
 
     if (!isset($services[$service])) {
